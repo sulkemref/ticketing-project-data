5663e6266dfc085573f2373740af15f1212b09c8
Kemail-terminal <sulkemref@gmail.com>
2023-04-21 14:19:53 -0500

    part 1 done

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -5,7 +5,7 @@ import com.cydeo.dto.RoleDTO;
 import java.util.List;
 
 public interface RoleService {
-    List<RoleDTO> listAllRoles();
+    public List<RoleDTO> listAllRoles();
     RoleDTO findById(Long id);
 
 }
