19c39d2a7f3e14ef439f01e9492450bfd28aa270
Kemail-terminal <sulkemref@gmail.com>
2023-04-25 10:33:42 -0500

    part 4 task entity done

---
+++ src/main/java/com/cydeo/entity/Task.java
@@ -0,0 +1,36 @@
+package com.cydeo.entity;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tasks")
+@Where(clause = "is_deleted=false")
+public class Task extends BaseEntity{
+
+    @OneToOne
+    private Project project;
+
+    @OneToOne
+    private User assignedEmployee;
+
+    private String taskSubject;
+    private String taskDetail;
+    @Enumerated(EnumType.STRING)
+    private Status taskStatus;
+    @Column(columnDefinition = "DATE")
+    private LocalDate assignedDate;
+}
